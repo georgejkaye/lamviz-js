d1b0130fd8c1a93daa2ce31ce64d8d4d376cff14
George Kaye <georgejkaye@gmail.com>
2018-10-04 15:44:41 +0100

    Exploring how to use JavaScript

---
+++ lambda.js
@@ -0,0 +1,12 @@
+
+/*
+Change the text of an object with a given id to the given text
+    doc: the document containing the object
+    id: the id of the object to change
+    text: the text to change it to
+*/
+function changeText(id, text){
+    document.getElementById(id).innerHTML = text;
+}
+
+
