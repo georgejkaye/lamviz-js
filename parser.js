b9e3afe2e60892a9e7225b5b85d1c5c16b09339e
George Kaye <georgejkaye@gmail.com>
2018-10-05 09:33:19 +0100

    More work on the parser

--- parser.js
+++ parser.js
@@ -4,6 +4,8 @@
 
 function parseTerm(text){
 
+    tokenise(text);
+
     const len = text.length;
 
     console.log("Input: " + text);
@@ -11,6 +13,66 @@ function parseTerm(text){
 
     var character = 0;
 
-    while c
+    var parseError = false;
+    var currentAbstraction = "";
+
+    while(character < len){
+    
+        while(index < len){
+
+            var currentCharacter = text.charAt(index);
+            
+            switch(currentCharacter){
+                
+                // beginning of a lambda abstraction
+                case '\\':
+                    abstraction = true;
+                    break;
+
+                // end of a lambda abstraction
+                case '.':
+                    if(abstraction){
+                        // TODO deal with abstraction
+                    } else {
+                        // dot with no associated lambda is a parse error
+                        parseError = true
+                        break;
+                    }
+
+                case '(':
+
+                    // TODO
+                    break;
+
+                case ')':
+
+                    // TODO
+                    break;
+
+                case ' ':
+                    break;
+
+                default:
+
+                    if (abstraction){
+                        currentAbstraction += currentCharacter;
+                    } else {
+
+                    // TODO
+
+                    }
+                    
+                    break;
+                
+
+    
+            }
+                
+    
+    
+        }
+    
+    
+    }
 
 }
