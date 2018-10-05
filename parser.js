991303a5f7f3709040966fc8fc33e0dacc28fc48
George Kaye <georgejkaye@gmail.com>
2018-10-05 10:28:11 +0100

    A tiny bit of work on the abstractions

--- parser.js
+++ parser.js
@@ -1,78 +1,77 @@
+import * as def from "definition";
+
 /**
  * Parse a lambda term
  */
 
 function parseTerm(text){
 
-    tokenise(text);
-
     const len = text.length;
 
     console.log("Input: " + text);
     console.log("Input length: " + len.toString());
 
-    var character = 0;
+    var index = 0;
 
     var parseError = false;
     var currentAbstraction = "";
 
-    while(character < len){
-    
-        while(index < len){
+    var term;
 
-            var currentCharacter = text.charAt(index);
+    while(index < len){
+
+        var currentCharacter = text.charAt(index);
+        
+        switch(currentCharacter){
             
-            switch(currentCharacter){
+            // beginning of a lambda abstraction
+            case '\\':
+                console.log("abstraction")
+                abstraction = true;
+                break;
+
+            // end of a lambda abstraction
+            case '.':
+                if(abstraction){
+                    // TODO deal with abstraction
+                    console.log(currentAbstraction);
+                } else {
+                    // dot with no associated lambda is a parse error
+                    parseError = true
+                    break;
+                }
+
+                break;
+
+            case '(':
+
+                // TODO
+                break;
+
+            case ')':
+
+                // TODO
+                break;
+
+            case ' ':
+                break;
+
+            default:
+
+                if (abstraction){
+                    currentAbstraction += currentCharacter;
+                } else {
+
+                // TODO
+
+                }
                 
-                // beginning of a lambda abstraction
-                case '\\':
-                    abstraction = true;
-                    break;
+                break;
+            
 
-                // end of a lambda abstraction
-                case '.':
-                    if(abstraction){
-                        // TODO deal with abstraction
-                    } else {
-                        // dot with no associated lambda is a parse error
-                        parseError = true
-                        break;
-                    }
-
-                case '(':
-
-                    // TODO
-                    break;
-
-                case ')':
-
-                    // TODO
-                    break;
-
-                case ' ':
-                    break;
-
-                default:
-
-                    if (abstraction){
-                        currentAbstraction += currentCharacter;
-                    } else {
-
-                    // TODO
-
-                    }
-                    
-                    break;
-                
-
-    
-            }
-                
-    
-    
-        }
-    
-    
+            
+        } 
+        
+        index++;
     }
-
 }
