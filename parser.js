a1e25119eb7d434ed3fcb88af182d9c107805a16
George Kaye <georgejkaye@gmail.com>
2018-10-05 13:37:32 +0100

    Subterm parsing implemented

--- parser.js
+++ parser.js
@@ -1,18 +1,56 @@
-import * as def from "definition";
+//import * as def from "definition";
+
+
+/**
+ * Find the index of the appropriate closing bracket - i.e. the end of the term
+ * @param {*} initial the initial index of the term in the whole string
+ * @param {*} text the text to search for the closing bracket
+ */
+function findClosingBracket(initial, text){
+
+    const len = text.length;
+    var index = 0;
+    var brackets = 1;
+
+    while(index < len){
+
+        var currentCharacter = text.charAt(index);
+
+        switch(currentCharacter){
+            case '(':
+                brackets++;
+                break;
+            case ')':
+                brackets--;
+                if(brackets === 0){
+                    return initial + index + 1;
+                }
+                break;
+            default:
+                break;
+        }
+
+        index++;
+    }
+
+    return -1;
+
+}
 
 /**
  * Parse a lambda term
+ * @param {*} text the text to parse for a lambda term
  */
-
 function parseTerm(text){
 
     const len = text.length;
 
-    console.log("Input: " + text);
-    console.log("Input length: " + len.toString());
+    console.log("Input to parse: " + text);
+    //console.log("Input length: " + len.toString());
 
     var index = 0;
 
+    var abstraction = false;
     var parseError = false;
     var currentAbstraction = "";
 
@@ -22,6 +60,8 @@ function parseTerm(text){
 
         var currentCharacter = text.charAt(index);
         
+        //console.log("Character " + index + ": " + currentCharacter);
+
         switch(currentCharacter){
             
             // beginning of a lambda abstraction
@@ -45,7 +85,16 @@ function parseTerm(text){
 
             case '(':
 
-                // TODO
+                const end = findClosingBracket(index, text.substring(index + 1));
+
+                if(end === -1){
+                    parseError = true;
+                }
+
+                console.log("Subterm: " + text.substring(index + 1, end));
+
+                var t1 = parseTerm(text.substring(index + 1, end));
+
                 break;
 
             case ')':
@@ -54,6 +103,7 @@ function parseTerm(text){
                 break;
 
             case ' ':
+                // spaces are permitted and can be skipped over
                 break;
 
             default:
