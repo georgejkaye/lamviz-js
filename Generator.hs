311451e3a7659082275b8a7a226a3991968b168a
George Kaye <georgejkaye@gmail.com>
2018-11-09 15:25:03 +0000

    Still mucking about with the generator

--- Generator.hs
+++ Generator.hs
@@ -16,5 +16,5 @@ data Term = Var Int | Abs Term | App Term Term
 enumerateTerms :: Int -> Int -> Int
 enumerateTerms 0 k = 0
 enumerateTerms n k = enumerateTerms (n-1) (k+1)
-                        + sum [(enumerateTerms n1 k) * (enumerateTerms (n-1-n1) k) | n1 <- [1..n-2]]
-                        + sum [k | n == 1] 
+                        + sum [(enumerateTerms n1 k) + (enumerateTerms (n-1-n1) k) | n1 <- [1..n-2]]
+                        + (if n == 1 then k else 0)
