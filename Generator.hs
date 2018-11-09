b4a387dcaa905fe0a0af2a0f32ccb8159e3784b0
George Kaye <georgejkaye@gmail.com>
2018-11-09 13:49:31 +0000

    Still playing around with enumeration

--- Generator.hs
+++ Generator.hs
@@ -14,7 +14,7 @@ data Term = Var Int | Abs Term | App Term Term
   Enumerate all the general lambda terms with subterms n and free variables k.
 -}
 enumerateTerms :: Int -> Int -> Int
-enumerateTerms 0 _ = 0
-enumerateTerms n k = enumerateTerms (n-1) (k+1)   -- abstraction case has one less subterm (the whole term) but one more free variable (the abstracted variable) 
+enumerateTerms 0 k = 0
+enumerateTerms n k = enumerateTerms (n-1) (k+1)
                         + sum [(enumerateTerms n1 k) * (enumerateTerms (n-1-n1) k) | n1 <- [1..n-2]]
                         + sum [k | n == 1] 
