95968fdb3f37cc33703816f0e157c2438229900c
George Kaye <georgejkaye@gmail.com>
2018-11-10 17:10:27 +0000

    Can now generate general lambda terms

--- Generator.hs
+++ Generator.hs
@@ -8,13 +8,22 @@ calculus
 -}
 module Generator where
 
-data Term = Var Int | Abs Term | App Term Term
+data Term = Var Int | Abs Term | App Term Term deriving (Show)
 
 {-|
   Enumerate all the general lambda terms with subterms n and free variables k.
 -}
 enumerateTerms :: Int -> Int -> Int
-enumerateTerms 0 k = 0
+enumerateTerms 0 _ = 0
+enumerateTerms 1 k = k
 enumerateTerms n k = enumerateTerms (n-1) (k+1)
-                        + sum [(enumerateTerms n1 k) + (enumerateTerms (n-1-n1) k) | n1 <- [1..n-2]]
-                        + (if n == 1 then k else 0)
+                         + sum [(enumerateTerms n1 k) * (enumerateTerms (n-1-n1) k) | n1 <- [1..n-2]]
+
+{-|
+  Generate all of the general lambda terms with subterms n and free variables k.
+-}
+generateTerms :: Int -> Int -> [Term]
+generateTerms 0 _ = []
+generateTerms 1 k = [Var x | x <- [1..k]]
+generateTerms n k = [Abs t | t <- generateTerms (n-1) (k+1)]
+                      ++ [App t1 t2 | n1 <- [1..n-2], t1 <- generateTerms n1 k, t2 <- generateTerms (n-1-n1) k]                     
