9a4b821bedf0ea763c091ac079d6ba0de6d4ad96
George Kaye <georgejkaye@gmail.com>
2018-12-11 12:30:26 +0000

    Planar term generator implemented but buggy

--- generator.js
+++ generator.js
@@ -53,3 +53,70 @@ function generateTerms(n, k){
     return terms;
 
 }
+
+/**
+ * Generate all planar lambda terms with a given number of subterms and free variables.
+ * @param {number} n - The number of subterms.
+ * @param {number} k - The number of free variables.
+ * @return {Object[]} The array of generated terms.
+ */
+function generatePlanarTerms(n, k){
+
+    if(typeof k === "number"){
+
+         var tempArray = [];
+
+         for(var i = k - 1; i >= 0; i--){
+             tempArray[k-i-1] = i;
+         }
+
+         k = tempArray;
+
+    }
+
+    var terms = [];
+
+    switch(n){
+        case 0:
+            break;
+        case 1:
+            
+            if(k.length === 1){
+                terms[0] = new LambdaVariable(k[0], "");
+            }
+
+            break;
+        default:
+
+            var absTerms = generatePlanarTerms(n-1, terms.map(function(e){return e + 1;}).concat([0]));
+
+            for(i = 0; i < absTerms.length; i++){
+                absTerms[i] = new LambdaAbstraction(absTerms[i], "");
+            }
+
+            var appTerms = [];
+            var x = 0;
+
+            for(var m = 1; m <= n-2; m++){
+                for(var i = 0; i <= k.length; i++){
+            
+
+                    var lhsTerms = generatePlanarTerms(m, k.slice(0, i));
+                    var rhsTerms = generatePlanarTerms(n-1-m, k.slice(i));
+
+                    for(a = 0; a < lhsTerms.length; a++){
+                        for(b = 0; b < rhsTerms.length; b++){
+                            appTerms[x] = new LambdaApplication(lhsTerms[a], rhsTerms[b]);
+                            x++;
+                        }
+                    }
+                }
+            }
+
+            terms = absTerms.concat(appTerms);
+
+    }
+
+    return terms;
+
+}
