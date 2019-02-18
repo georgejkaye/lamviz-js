1add6bdc1d04e4c8eb23af8e48cacf8d7894df2f
George Kaye <georgejkaye@gmail.com>
2019-02-18 15:52:21 +0000

    The normalisation tree displays well now!

--- graph.js
+++ graph.js
@@ -24,7 +24,7 @@ const nodeDistanceY = 30;
 /** The distance between adjacent normalisation nodes in the X direction. */
 const normalisationDistanceX = 500;
 /** The distance between adjacent normalisation nodes in the Y direction. */
-const normalisationDistanceY = 1000;
+const normalisationDistanceY = 400;
 /** The width of one normalisation node. */
 const normalisationNodeWidth = 150;
 
@@ -731,6 +731,50 @@ function checkForIdenticalReduction(id, source, target){
 }
 
 /**
+ * Perform adjustments on a map to ready it for being the child of a normalisation node.
+ * @param {Object[]} termElems - The elements generated for the map of the term.
+ * @param {string} nodeID - The node that this map belongs to.
+ * @return {Object []} The terms adjusted for prettiness.
+ */
+function setupNormalisationNode(termElems, nodeID){
+    var highest = 0;
+
+    for(var i = 0; i < termElems.length; i++){
+        
+        if(termElems[i].group === "nodes"){
+        
+            /* Make this node a child of the normalisation node */
+            termElems[i].data.parent = nodeID;
+
+            const y = termElems[i].position.y;
+
+            if(y < highest){
+                highest = y;
+            }
+
+        }
+
+        /* Associate elements with this node to avoid id clashes later */
+        termElems[i].data.id += "_child_of_" + nodeID;
+
+        /* Make sure the source and target point to the new ids */
+        if(termElems[i].group === "edges"){
+            termElems[i].data.source += "_child_of_" + nodeID;
+            termElems[i].data.target += "_child_of_" + nodeID;
+        }
+    }
+
+    /* Extend all variable paths to the top to avoid unnecessary crossings */
+    for(var i = 0; i < termElems.length; i++){
+        if(termElems[i].data.type === varNodeTop || termElems[i].data.type === absNodeFree){
+            termElems[i].position.y = highest - nodeDistanceY / 2;
+        }
+    }
+
+    return termElems;
+}
+
+/**
  * Generate the elements of the normalisation graph.
  * @param {string} id - The id of where the graph will be drawn.
  * @param {Object} tree - The normalisation tree object.
@@ -748,43 +792,27 @@ function generateNormalisationGraphElements(id, tree, ctx, parent, parentReducti
         level = 0;
     }
 
+    /* Nodes use de Bruijn notation to ensure only distinct ones are created (i.e. not alpha-equivalent) */
     var nodeID = tree.term.prettyPrint();
-
     tree.term.generatePrettyVariableNames(ctx);
 
-    /*var mapPic = drawMap(id, tree.term, ctx).png;
-    document.querySelector('#png-eg').setAttribute('src', mapPic);*/
-
+    /* Draw the term represented by this normalisation node */
     var termElems = generateMapElements(tree.term, ctx);
-
-    console.log(termElems);
-
-    for(var i = 0; i < termElems.length; i++){
-        
-        if(termElems[i].group === "nodes"){
-            termElems[i].data.parent = nodeID;
-        }
-
-        if(termElems[i].group === "edges"){
-            termElems[i].data.source += "_child_of_" + nodeID;
-            termElems[i].data.target += "_child_of_" + nodeID;
-        }
-
-        termElems[i].data.id += "_child_of_" + nodeID;
-    }
+    termElems = setupNormalisationNode(termElems, nodeID);
 
     array = array.concat(termElems);
 
-    console.log(termElems);
-
+    /* Define the node */
     array = defineNode(array, nodeID, "norm", 0, level * normalisationDistanceY, tree.term.prettyPrintLabels(), level);  
 
+    /* Generate the elements for the various branches from this node */
     for(var i = 0; i < tree.reductions.length; i++){
         array = array.concat(generateNormalisationGraphElements(id, tree.reductions[i][0], ctx, nodeID, tree.reductions[i][1].prettyPrintLabels(), level + 1));
     }
 
+    /* Only define edges that represent distinct reductions (i.e. not alpha-equivalent) */
     if(parent !== undefined && !checkForIdenticalReduction(parentReduction, parent, nodeID)){
-        array = defineEdge(array, checkID(parentReduction, edges), "", parent, nodeID, parentReduction);
+        array = defineEdge(array, checkID(parentReduction, edges), "norm", parent, nodeID, parentReduction);
     }
 
     return array;
@@ -851,17 +879,16 @@ function drawNormalisationGraph(id, term, ctx){
                 'arrow-scale': '0.8',
                 'font-size': '2',
                 'curve-style': 'bezier',
-                'label': function(ele){
-                    return ele.data().label;
-                },
+                'label': ""
                 }
             },
 
             {
                 selector: 'edge[type="norm"]',
                 style: {
-                'target-arrow-color': '#ccc',
-                'target-arrow-shape': 'triangle'
+                'target-arrow-color': 'black',
+                'target-arrow-shape': 'triangle',
+                'line-color': 'black',
                 }
             },
 
@@ -901,9 +928,8 @@ function drawNormalisationGraph(id, term, ctx){
 
         var c = Math.floor(elems.length / 2);
 
-
         for(var j = 0; j < elems.length; j++){
-            
+
             var posX = 0;
             
             if(elems.length % 2 === 0){
@@ -913,8 +939,11 @@ function drawNormalisationGraph(id, term, ctx){
             }
 
             elems[j].position('x', posX);
+            elems[j].position('y', i * normalisationDistanceY);
         }
         
     }
+
+    cy.fit(cy.filter(function(ele, i, eles){return true;}), 10);
 
 }
