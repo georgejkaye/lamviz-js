ea980758ae1b82e22c418b5c5bd6ec55774317c2
George Kaye <georgejkaye@gmail.com>
2021-09-09 00:31:02 +0100

    Added some icons to the sidebar

--- src/ts/sidebar/Sidebar.tsx
+++ src/ts/sidebar/Sidebar.tsx
@@ -5,6 +5,8 @@ import { Mode, changeMode } from "./sidebarSlice"
 import GallerySidebar from "./GallerySidebar"
 import { sidebarWidth } from "../workbench/workbenchSlice"
 
+import Github from "../../data/svgs/github.svg"
+
 export default function Sidebar() {
 
     let dispatch = useAppDispatch()
@@ -15,25 +17,12 @@ export default function Sidebar() {
         dispatch(changeMode(mode))
     }
 
-    // return (
-    //     <div className="sidebar">
-    //         <div className="sidebar-heading">Welcome to the</div>
-    //         <div className="sidebar-title">Lambda {mode === Mode.VISUALISER ? "visualiser" : "gallery"}</div>
-    //         <div className="sidebar-content sidebar-text">
-    //             <div>This project was first developed as my dissertation from my undergraduate project supervised by <a href="https://noamz.org">Noam Zeilberger</a>, and later improved in my spare time. <a href="https://www.georgejkaye.com/lambda-visualiser">Read more</a>.</div>
-    //         </div>
-    //         <div className="sidebar-content sidebar-text">
-    //             <div className="credit">Graph drawing powered by <a href="https://js.cytoscape.org">Cytoscape.js</a>.</div>
-    //             <div className="credit">Parsing adapted from <a href="https://github.com/tadeuzagallo/lc-js">lc-js</a>.</div>
-    //         </div>
-    //         <div className="tabs">
-    //             <span className={"tab " + (mode === Mode.VISUALISER ? "active-tab" : "inactive-tab")} onClick={(e) => setMode(Mode.VISUALISER)}>Visualiser</span>
-    //             <span className={"tab " + (mode === Mode.GALLERY ? "active-tab" : "inactive-tab")} onClick={(e) => setMode(Mode.GALLERY)}>Gallery</span>
-    //         </div>
-    //         {mode === Mode.VISUALISER ? <VisualiserSidebar /> : <GallerySidebar />}
-
-    //     </div >)
     return (
-        <div style={{ width: sidebarWidth }}></ div >
+        <div className="sidebar" style={{ width: sidebarWidth }}>
+            <div className="sidebar-block"><a href="https://www.georgejkaye.com/lambda-visualiser" title="Details about the visualiser"><span className="sidebar-icon">λ</span></a></div>
+            <div className="spacer"></div>
+            <div className="sidebar-block"><a href="https://github.com/georgejkaye/lamviz" title="GitHub repository for this project"><img className="sidebar-icon" src={Github} alt="GitHub logo" /></a></div>
+
+        </ div >
     )
 }
